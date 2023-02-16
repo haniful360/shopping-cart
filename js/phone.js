aeb6cd983a0d3cb36a70af9d4817af4cfab204b7
Haniful Islam <haniful360@gmail.com>
2023-02-16 18:39:30 +0600

    phone price total added

--- js/phone.js
+++ js/phone.js
@@ -1,5 +1,36 @@
+function updatePhoneNumber(isIncrease) {
+    const phoneInputfield = document.getElementById('phone-number-field')
+    const phoneInputText = phoneInputfield.value;
+    const previousPhoneNumber = parseInt(phoneInputText);
 
+    let newPhoneNumber;
+    if (isIncrease === true) {
+        newPhoneNumber = previousPhoneNumber + 1;
+    }
+    else {
+        newPhoneNumber = previousPhoneNumber - 1;
+    }
+    phoneInputfield.value = newPhoneNumber;
+    return newPhoneNumber;
+}
 
+function updatePhoneNumberPrice(newPhoneNumber) {
+    const phoneTotalPrice = newPhoneNumber * 1219;
+    const phoneTotalElement = document.getElementById('phone-total')
+    phoneTotalElement.innerText = phoneTotalPrice;
+}
+
+document.getElementById('phone-plus-btn').addEventListener('click', function () {
+
+    const newPhoneNumber = updatePhoneNumber(true);
+    updatePhoneNumberPrice(newPhoneNumber);
+
+})
+document.getElementById('phone-minus-btn').addEventListener('click', function () {
+    
+    const newPhoneNumber = updatePhoneNumber(false);
+    updatePhoneNumberPrice(newPhoneNumber);
+})
 
 
 // document.getElementById('phone-plus-btn').addEventListener('click', function(){
